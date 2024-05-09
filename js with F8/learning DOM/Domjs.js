b332551610d8fa43bf6019221dfb5e83c9443f3f
danphi <phittnh@gmail.com>
2024-05-09 12:02:25 +0700

    get element

---
+++ js with F8/learning DOM/Domjs.js
@@ -0,0 +1,11 @@
+// HTML DOM
+
+//1. Element: get qua ID, Class, tag ,CSS seletor, HTML collection
+var heddingNode = document.getElementById('headding')
+console.log(heddingNode)
+var querySelectos = document.querySelector('.headding-2')
+console.log(querySelectos)
+var querySelectorChildLast = document.querySelector('.box .headding-2:nth-child(2)')
+console.log(querySelectorChildLast)
+//2 Attributw
+//3.Text
