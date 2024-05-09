4df330ba2cfa7b663f470a797152bc8061e2e701
danphi <phittnh@gmail.com>
2024-05-09 12:11:07 +0700

    get element2

--- js with F8/learning DOM/Domjs.js
+++ js with F8/learning DOM/Domjs.js
@@ -7,5 +7,9 @@ var querySelectos = document.querySelector('.headding-2')
 console.log(querySelectos)
 var querySelectorChildLast = document.querySelector('.box .headding-2:nth-child(2)')
 console.log(querySelectorChildLast)
+
+var box1 = document.querySelector('.box-1')
+var li = box1.querySelectorAll('li')
+console.log(li)
 //2 Attributw
 //3.Text
