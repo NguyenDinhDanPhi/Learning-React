9cb5d953954e3fcf76de667174bbb235dd01d1e5
danphi <phittnh@gmail.com>
2024-05-10 17:48:29 +0700

    Event listener

--- js with F8/learning DOM/EvenListener.js
+++ js with F8/learning DOM/EvenListener.js
@@ -1,7 +1,14 @@
 var btn = document.getElementById('btn')
-btn.onclick = (e) => {
+function viec1() {
     console.log("viec 1")
-    console.log("viec 2")
-    alert("viec 3")
 }
-alert("viec 3")
+
+function viec2() {
+    console.log("viec 2")
+}
+btn.addEventListener('click', viec1)
+btn.addEventListener('click', viec2)
+
+setTimeout(() => {
+    btn.removeEventListener('click',viec1)
+},3000)
