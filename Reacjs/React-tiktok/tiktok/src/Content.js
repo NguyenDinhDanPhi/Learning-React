4764886426a3095a14be5f56ebb5785f14e1de2e
danphi <phittnh@gmail.com>
2024-06-07 11:11:52 +0700

    useEffect() with DOM events

--- Reacjs/React-tiktok/tiktok/src/Content.js
+++ Reacjs/React-tiktok/tiktok/src/Content.js
@@ -11,17 +11,32 @@ function Content() {
 
 // ----------------------------------------------------------------
     //1. callbacks luôn được gọi sau khi component mount
+    //2. cleanUp funcion luon duoc goi truoc khi component unmount
     const tabs = ["posts","comments", 'albums']
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
     const [type, setType] = useState('posts')
+    const [showGoToTop, setShowGoToTop] = useState(false)
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
             .then(posts => {setPosts(posts)})
     }, [type])
    
-
+    useEffect(() => {
+        const handlerScroll = () => {
+            if (window.scrollY >= 200) {
+                setShowGoToTop(true)
+            } else {
+                setShowGoToTop(false)
+            }
+        }
+        window.addEventListener('scroll', handlerScroll)
+    // cleanup functions
+        return () => {
+        window.removeEventListener('scroll', handlerScroll)
+        }    
+    }, [])
     return (
         <div>
            
@@ -44,6 +59,21 @@ function Content() {
                     </li>
                 ))}
             </ul>
+
+            {showGoToTop && (
+                <button 
+                style={
+                    {
+                        color: '#fff',
+                        backgroundColor: '#333',
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20
+                    }
+                }>
+                    Go to Top
+                </button>
+            )}
         </div>
         
     );
