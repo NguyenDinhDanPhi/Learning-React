c1e81ee662c3aa75c44db7779054a196503e4f21
danphi <phittnh@gmail.com>
2024-06-07 12:29:23 +0700

    useLayoutEffect

--- Reacjs/React-tiktok/tiktok/src/FakeChatApp.js
+++ Reacjs/React-tiktok/tiktok/src/FakeChatApp.js
@@ -1,6 +1,20 @@
 import { useEffect, useState } from "react";
 
+//  sự khác nhau của useEffect và useLayoutEffect
+// useEffect
+// 1. Cập nhật state
+// 2. Cập nhật DOM (mutated)
+// 3. Rẹnder UI
+// 4. Gọi cleanup nếu deps thay đổi
+// 5. Gọi useEffect callback
+// -----------------------------------
 
+// useLayoutEffect
+// 1. Cập nhật state
+// 2. Cập nhật DOM (mutated)
+// 3. Gọi cleanup nếu deps thay đổi (sync)
+// 4. GỌi useLayoutEffect callback (sync)
+// 5. Reander Ui
 const lessons = [
     {
         id: 1,
